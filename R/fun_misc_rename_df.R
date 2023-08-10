c2c1799379afcf0a12abaa186a7a4fead1c78c0d
Cao Bittencourt Ferreira <caobittencourt@gmail.com>
2023-08-10 17:57:14 -0300

    added fun_misc_str_standard function

--- R/fun_misc_rename_df.R
+++ R/fun_misc_rename_df.R
@@ -15,6 +15,41 @@
 #
 #
 # [FUNCTIONS] -------------------------------------------------------------
+# - String standardization function ---------------------------------------
+fun_misc_str_standard <- function(chr_string){
+
+  # Coerce arguments to string
+  chr_string %>%
+    sapply(as.character) %>%
+    str_trim() %>%
+    str_to_lower() %>%
+    str_remove_all(
+      '\\.$'
+    ) %>%
+    str_replace_all(
+      '\\.', '_'
+    ) %>%
+    str_replace_all(
+      ' ', '_'
+    ) %>%
+    str_replace_all(
+      '-', '_'
+    ) %>%
+    str_replace_all(
+      '/', '_'
+    ) %>%
+    str_remove_all(
+      ','
+    ) %>%
+    str_remove_all(
+      "'"
+    ) -> chr_string
+
+  # Output
+  return(chr_string)
+
+}
+
 # - Rename data frame -----------------------------------------------------
 fun_misc_rename_df <- function(df_data){
 
@@ -28,33 +63,10 @@ fun_misc_rename_df <- function(df_data){
   df_data %>%
     rename_with(
       .cols = everything()
-      , .fn = ~ .x %>%
-        str_trim() %>%
-        str_to_lower() %>%
-        str_replace_all(
-          '\\.', '_'
-        ) %>%
-        str_replace_all(
-          ' ', '_'
-        ) %>%
-        str_replace_all(
-          '-', '_'
-        ) %>%
-        str_replace_all(
-          '/', '_'
-        ) %>%
-        str_remove_all(
-          ','
-        ) %>%
-        str_remove_all(
-          "'"
-        )
+      , .fn = fun_misc_str_standard
     ) -> df_data
 
   # Output
   return(df_data)
 
 }
-
-
-
